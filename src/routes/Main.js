03356d14348a037c2c73238e848a4c59872056f0
Jongu Yu <yujongu@gmail.com>
2020-10-13 14:50:59 +0900

    First Commit!!

---
+++ src/routes/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Music Together</h1>
+      </header>
+    );
+  }
+}
+
+export default Main;
